3e99ba6ae90f3009db51bbcd6b2fc170a010d9db
erickhonorio5 <erick.honorio40@gmail.com>
2025-08-22 17:13:10 -0300

    JWT Login e Register feito

--- src/main/java/rct/sistema/backend/controllers/dto/request/LoginRequestDTO.java
+++ src/main/java/rct/sistema/backend/controllers/dto/request/LoginRequestDTO.java
@@ -2,7 +2,6 @@ package rct.sistema.backend.controllers.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
